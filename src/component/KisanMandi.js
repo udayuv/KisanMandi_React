3dec8e1eac48936bc93462b0562cd5d6714b7730
udayuv <uday.lko67@gmail.com>
2020-06-12 12:02:01 +0530

    created the structure

---
+++ src/component/KisanMandi.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function KisanMandi() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Learn React
+      </header>
+    </div>
+  );
+}
+
+export default KisanMandi;
